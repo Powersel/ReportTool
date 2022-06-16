447aa5169f2240673a5749a2ab275ffb658e072f
Sergiy.Shevchuk <Sergiy.Shevchuk@icloud.com>
2022-06-16 18:43:06 +0300

    Added custom init

--- Sources/RokuReportTool/RokuReportToolViewController.swift
+++ Sources/RokuReportTool/RokuReportToolViewController.swift
@@ -5,11 +5,17 @@ typealias ButtonActionCompletionHandler = () -> ()
 
 final public class RokuReportToolViewController: RokuBaseViewController {
     
+    init(with value: String) {
+        super.init(nibName: "RokuReportToolViewController", bundle: .module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         print("RokuReportToolViewController is loaded")
     }
-    
 }
