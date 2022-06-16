cf30a0d12b82c1849d8324527dd8f59f3ff9316f
Sergiy.Shevchuk <Sergiy.Shevchuk@icloud.com>
2022-06-16 13:07:24 +0300

    Added test structs

---
+++ Sources/RepoTestA.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Powersel on 16.06.2022.
+//
+
+import Foundation
+
+struct RepoTestA {
+    
+    func simplePrint() {
+        print("Simple AAAA")
+    }
+    
+    init() {
+        
+    }
+}
---
+++ Sources/RokuReportTool/RepoTestB.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Powersel on 16.06.2022.
+//
+
+import Foundation
+
+import Foundation
+
+struct RepoTestB {
+    
+    func simplePrint() {
+        print("Simple BBB")
+    }
+    
+    init() {
+        
+    }
+}
